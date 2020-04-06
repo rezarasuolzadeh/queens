e34d1b0556e41a8b080772f107c20b3979f49dd9
RezaRasoulzadeh <reza.rassoulzadeh@gmail.com>
2020-04-06 17:04:44 +0430

    create exit dialog.

--- app/src/main/java/ir/rezarasoulzadeh/queens/activity/MainActivity.kt
+++ app/src/main/java/ir/rezarasoulzadeh/queens/activity/MainActivity.kt
@@ -1,9 +1,12 @@
 package ir.rezarasoulzadeh.queens.activity
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.DisplayMetrics
-import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
+import android.view.LayoutInflater
 import android.widget.GridView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import ir.rezarasoulzadeh.queens.R
 import ir.rezarasoulzadeh.queens.adapter.ChessBoardAdapter
@@ -11,6 +14,7 @@ import ir.rezarasoulzadeh.queens.evaluate.QueensLocations
 import ir.rezarasoulzadeh.queens.solution.Queen
 import ir.rezarasoulzadeh.queens.toast.CustomToast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_for_exit.view.*
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
@@ -24,7 +28,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get device dimensions
         val displayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
 
@@ -45,7 +48,7 @@ class MainActivity : AppCompatActivity() {
         customToast = CustomToast(context, inflater)
 
         plusButton.setOnClickListener {
-            if(queenNumberTextView.text.toString().toInt() == 40) {
+            if (queenNumberTextView.text.toString().toInt() == 40) {
                 customToast.show("امکان پذیر نیست", "short")
             } else {
                 val currentQueen = queenNumberTextView.text.toString().toInt()
@@ -69,6 +72,22 @@ class MainActivity : AppCompatActivity() {
             gridView.numColumns = queensCount
             gridView.adapter = ChessBoardAdapter(this, queensLocations, width)
         }
-
     }
+
+    override fun onBackPressed() {
+        val exitView = LayoutInflater.from(this).inflate(R.layout.dialog_for_exit, null)
+
+        val exitViewBuilder = this.let { it1 -> AlertDialog.Builder(it1).setView(exitView) }
+
+        val exitAlertDialog = exitViewBuilder.show()
+
+        exitAlertDialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        exitAlertDialog.setCanceledOnTouchOutside(false)
+
+        exitView.exitButton.setOnClickListener {
+            this.finish()
+        }
+    }
+
 }

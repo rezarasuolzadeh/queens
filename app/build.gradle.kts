469d8fe427e41804952bae9fc66bd8988c26f8ea
reza.rasoulzadeh <reza.rasoulzadeh@eniac-tech.local>
2025-05-21 09:03:30 +0330

    upgrade version code and version name.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ir.rezarasuolzadeh.queens"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
